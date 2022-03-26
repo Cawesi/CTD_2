d59e47764c8e790dfdc3bbd3bcf6cc485ce19c9b
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-03-25 21:47:34 -0300

    jogada 01

---
+++ Front_II/Aulas/03/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,25 @@
+let tabuleiro = document.querySelectorAll('.coordenada');
+
+// Jogada 01
+tabuleiro[0].innerText = "X";
+
+// Jogada 02
+tabuleiro[1].innerText = "X";
+
+// // Jogada 03
+// tabuleiro[0].innerText = "X";
+
+// // Jogada 04
+// tabuleiro[0].innerText = "X";
+
+// // Jogada 05
+// tabuleiro[0].innerText = "X";
+
+// // Jogada 06
+// tabuleiro[0].innerText = "X";
+
+// // Jogada 07
+// tabuleiro[0].innerText = "X";
+
+// // Jogada 08
+// tabuleiro[0].innerText = "X";
