d146f29f2eb31185e8d8abbe6c8992c3b570ecda
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-03-27 18:43:39 -0300

    Mesa de trabalho - Aula_08

--- Front_II/Aulas/08/mesa-de-trabalho/assets/js/main.js
+++ Front_II/Aulas/08/mesa-de-trabalho/assets/js/main.js
@@ -1,2 +1,42 @@
+const tela = document.body;
+const tela_inicial = document.querySelector('#telaInicial');
+const btn_iniciar_jogo = document.querySelector('.botao');
+const tabuleiro = document.querySelectorAll('.coordenada');
 
-// Utilize esse documento para implementar seu algorítimo...
+
+btn_iniciar_jogo.addEventListener('click', function(event){
+
+    tela_inicial.classList.toggle('tela_de_bloqueio');
+    document.querySelector('.tela_de_bloqueio').removeAttribute('id');
+});
+
+tela.addEventListener('keydown', function(event){
+
+    if (event.code == "Space" || event.code == "Enter"){
+
+        tela_inicial.classList.toggle('tela_de_bloqueio');
+        document.querySelector('.tela_de_bloqueio').removeAttribute('id');
+    };
+
+    if (event.code == "Escape"){
+
+        document.querySelector('.tela_de_bloqueio').setAttribute('id', 'telaInicial');
+        document.querySelector('#telaInicial').classList.toggle('tela_de_bloqueio');
+    }
+});
+
+let tab = document.querySelector('.tabuleiro');
+
+tab.addEventListener('click', function(event){
+
+    let clique = event.target;
+
+    clique.innerText = 'X';
+})
+
+tab.addEventListener('dblclick', function(event){
+
+    let clique = event.target;
+
+    clique.innerText = 'O';
+})
