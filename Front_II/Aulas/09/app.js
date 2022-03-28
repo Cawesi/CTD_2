a062a0dc44d2027b9a94ba8c84d323d3d74be64a
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-03-28 20:44:24 -0300

    Mesa de trabalho - Aula_09

--- Front_II/Aulas/09/app.js
+++ Front_II/Aulas/09/app.js
@@ -1,10 +1,41 @@
-(function() {
+const abrir_menu = document.body;
+let sidebar = document.querySelector('.l-sidebar');
+let btn_about_me = document.querySelector('.c-btn');
+let btn_menu = document.querySelector('.l-sidebar__btn');
+
+
+abrir_menu.addEventListener('keydown', (event) => {
+
+    if(event.code == "Space"){
+        let valor_classe = sidebar.getAttribute('class');
     
-    // Adicione sua implementação aqui...
-    // Tem total liberdade de alterar o documento HTML, CSS e JS.
+        if(valor_classe.indexOf('l-sidebar--close') == -1){
 
+            sidebar.classList.toggle('l-sidebar--open');
+        } else {
 
-    // Um ótimo feedback para todos.
+            sidebar.classList.remove('l-sidebar--close');
+            sidebar.classList.toggle('l-sidebar--open');
+        };
+    };
+})
 
+btn_menu.addEventListener('click', function(event){
 
-})();
+    let valor_classe = sidebar.getAttribute('class');
+    
+    if(valor_classe.indexOf('l-sidebar--close') == -1){
+
+        sidebar.classList.toggle('l-sidebar--open');
+    } else {
+
+        sidebar.classList.remove('l-sidebar--close');
+        sidebar.classList.toggle('l-sidebar--open');
+    };
+});
+
+sidebar.addEventListener('click', () => { 
+
+    sidebar.classList.toggle('l-sidebar--close');
+    sidebar.classList.remove('l-sidebar--open');
+});
