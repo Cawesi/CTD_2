6eb8b29b33e4310e9d7f6077ddafd71475ecd361
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-03-28 23:05:41 -0300

    Mesa de trabalho - Aula_10

--- Front_II/Aulas/10/mesa-de-trabalho/assets/js/main.js
+++ Front_II/Aulas/10/mesa-de-trabalho/assets/js/main.js
@@ -1,10 +1,35 @@
 
 // 1. Selecione o formulário
+const formulario = document.querySelector('#formularioDeCadastro');
+let lista = document.querySelector('.c-lista');
 
 // 2. Crie da rotina que será disparada a partir do evento de envio
-
 // 2.2. Remova todos os espaços vazios
-
 // 2.1. Remova todos os números permitindo apenas texto
 
-// 3. Capture o evento de envio do formulário e disparar a rotina
+
+formulario.addEventListener('submit', function(event){
+
+    let valor_imput = event.target[0].value;
+
+    event.preventDefault()
+
+    let valor = valor_imput.trim();
+
+    let re = /\d/g;
+
+    if(!re.test(valor_imput)){
+
+        lista.innerHTML = `<li class="c-lista__item"> ${valor}</li>`;
+    } else {
+
+        valor = valor.replace(re, "");
+        
+        valor = valor.trim();
+        lista.innerHTML = `<li class="c-lista__item"> ${valor}</li>`;  // 3. Capture o evento de envio do formulário e disparar a rotina
+    }
+})
+
+
+
+
