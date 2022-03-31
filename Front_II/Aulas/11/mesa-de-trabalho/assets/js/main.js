ecc94e696e9e80e70f96bffb5a2da8f777ab6635
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-03-30 22:58:29 -0300

    Aula 11 - Mesa

--- Front_II/Aulas/11/mesa-de-trabalho/assets/js/main.js
+++ Front_II/Aulas/11/mesa-de-trabalho/assets/js/main.js
@@ -1,18 +1,5 @@
 
 /* 
-
-    1. Selecione o formulário e o botão do campo de entrada
-
-    2. Crie uma rotina que será disparada a partir do evento de envio
-
-        - 2.1. Remova todos os espaços vazios no início e no fim do texto
-
-        - 2.2. Valide se existem caracteres especiais
-
-            - Se o campo possuir caracteres especiais, apresente um alerta para o usuário com a seguinte mensagem `"Não é permitido o uso de caracteres especiais em sua lista."`. **Nota:** Pode ser utilizado o método `alert()` para simplificar a lógica.
-
-        - 2.3. Caso passe na validacão, apresente o item no elemento DOM `<li class="c-lista__item">`.
-
     3. Crie uma rotina que será disparada sempre que o usuário tirar o foco da campo de entrada
 
         - 2.1. Valide se o campo esta vazio
@@ -23,3 +10,42 @@
 
             - Se o campo possuir caracteres especiais, apresente a seguinte mensagem no console do navegador `"Não é permitido o uso de caracteres especiais em sua lista."`.
 */
+
+// Selecione o formulário e o botão do campo de entrada
+let formulario = document.forms[0];
+
+// Valide se existem caracteres especiais
+function tem_caracter_especial(valor){
+
+    // Remova todos os espaços vazios no início e no fim do texto
+    let valor_tratado =  valor.trim();
+
+    let regx_carac_espec = /\W/g;
+    let regx_space = /\s/g;
+    valor_tratado = valor.replace(regx_space, "");
+    valor_tratado = regx_carac_espec.test(valor_tratado);
+
+    return valor_tratado;
+};
+
+
+// Crie uma rotina que será disparada a partir do evento de envio
+formulario.addEventListener('submit', function(event){ 
+    
+    event.preventDefault();
+    let valor_imput = event.target['item'].value;
+    //  Se o campo possuir caracteres especiais, apresente um alerta para o usuário com a seguinte mensagem 
+    //  "Não é permitido o uso de caracteres especiais em sua lista."
+    if(tem_caracter_especial(valor_imput)){
+
+        alert("Não é permitido o uso de caracteres especiais em sua lista.");
+    } else {
+
+        // Caso passe na validacão, apresente o item no elemento DOM `<li class="c-lista__item">`.
+        let lista = document.querySelector('.c-lista');
+        let li = document.createElement('li');
+        li.classList.add('c-lista__item');
+        li.innerText = valor_imput;
+        lista.appendChild(li);
+    };
+});
