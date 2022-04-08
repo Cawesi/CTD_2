27bf01df29c435d8b3599f4b6a87d941dd831488
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-04-08 19:20:07 -0300

    Aula 13

--- Front_II/Aulas/12/mesa-de-trabalho/assets/js/main.js
+++ Front_II/Aulas/12/mesa-de-trabalho/assets/js/main.js
@@ -1,16 +1,3 @@
-
-var formulario = document.forms['formularioDeCadastro'];
-
-formulario.addEventListener('submit', function(evento) {
-
-    var valorDoCampoItem = evento.target['item'].value.trim();
-    
-    // 1. Crie um objeto literal para armazenar os itens considerando as propriedades: id, tarefa, autor.
-    
-    // 2. Converta o objeto literal em um json.
-
-    // 3. Armazene o json no localStorage ou sessionStorage.
-
-});
-
+//  1. Crie um `objeto literal` para armazenar os itens considerando as propriedades: `id`, `tarefa`, `autor`.
+//    1.1. Crie um método no `objeto literal` que captura a data e hora atual e armaneze na propriedade `data`
 
