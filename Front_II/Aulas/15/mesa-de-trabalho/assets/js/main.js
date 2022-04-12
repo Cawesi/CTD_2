22e1cb2174de3a8e3e06c3e07a6a658ec186839b
Cawesi <wellingtonsiqueira.santos@gmail.com>
2022-04-12 20:56:34 -0300

    Mesa de Trabalho - Aula 15

--- Front_II/Aulas/15/mesa-de-trabalho/assets/js/main.js
+++ Front_II/Aulas/15/mesa-de-trabalho/assets/js/main.js
@@ -1,16 +1,29 @@
 
-var formulario = document.forms['formularioDeCadastro'];
+var formulario = document.forms[0];
 
-formulario.addEventListener('submit', function(evento) {
+formulario.addEventListener('submit', function(event) {
 
-    var valorDoCampoItem = evento.target['item'].value.trim();
+    event.preventDefault()
     
     // 1. Crie um objeto literal para armazenar os itens considerando as propriedades: id, tarefa, autor.
-    
-    // 2. Converta o objeto literal em um json.
+    fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+        .then((response) => {return response.json()})
+        .then((tarefa) => {
 
-    // 3. Armazene o json no localStorage ou sessionStorage.
+            let lista_ul_pai = document.querySelector('.c-lista');
+            let lista_li_remove = document.querySelector('.c-lista__item');
+            lista_ul_pai.removeChild(lista_li_remove);
+
+            tarefa.forEach(element => {
+                let emoji = '😄';
+                let lista_ul = document.querySelector('.c-lista');
+                let lista_li = document.createElement('li');
+                lista_li.classList.add('c-lista__item');
+                lista_li.innerText = emoji + ' ' + element.title;
+                lista_ul.appendChild(lista_li);
+        });
+    })
+
+    
 
 });
-
-
